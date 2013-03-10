8b109c55380f0e327f10ded41839a28bc2561fe4
bret <bret@bret-ThinkPad-T530.(none)>
2013-03-10 17:23:57 -0500

    Removed kludgy "safeMode" option. Users are now on their own to attempt to test for correct display of accidental symbols.

--- jquery.accidentals.js
+++ jquery.accidentals.js
@@ -24,36 +24,29 @@
 				doubleFlat: {
 					text: '-double-flat',
 					'b#': 'bb',
-					replacement: '\uD834\uDD2B',
-					safe: true
+					replacement: '\uD834\uDD2B'
 				},
 				doubleSharp: { //until I learn how to write better regular expressions, we need to search for ## before we search for #
 					text: '-double-sharp',
 					'b#': '##',
-					replacement: '\uD834\uDD2A',
-					safe: true
+					replacement: '\uD834\uDD2A'
 				},
 				natural: {
 					text: '-natural',
-					'b#': 'nat', //used to be 'n|nat' - changed because it converts the common word 'an'
-					replacement: '\u266E',
-					safe: true
+					'b#': 'nat', //used to be 'n|nat' - changed because that wrongly converts the common word 'an'
+					replacement: '\u266E'
 				},
 				flat: {
 					text: '-flat',
 					'b#': 'b',
-					replacement: '\u266D',
-					safe: true
+					replacement: '\u266D'
 				},
 				sharp: {
 					text: '-sharp',
 					'b#': '#',
-					replacement: '\u266F',
-					safe: true
+					replacement: '\u266F'
 				}
 			},
-			knownSafeSingle = ['iPod', 'iPhone', 'iPad'], //these fail the safeMode test, but do actually have symbols available
-			//knownSafeDouble = [], //none known
 			defaults = {
 				accidentalClassBase: 'accidental',
 				accidentalWrap: '<span class="accidental" />',
@@ -62,29 +55,11 @@
 				inputTypes: ['text'],
 				noteNames: '[A-H]',
 				noteNameWrap: '',
-				outerWrap: '',
-				safeMode: true
+				outerWrap: ''
 			};
 
 		options = $.extend(true, defaults, options); // deep replace = true
 		options.caseSensitive = options.caseSensitive ? '' : 'i';
-
-		if (options.safeMode) {
-			$testDiv = $('<div style="visibility: hidden" />').appendTo($(this).first());
-			forEachIn(accidentals, function (k, v) {
-				$testDiv.append('<span style="font-size: 999px; margin: 0; padding: 0;" class="' + k + '">' + v.replacement + '</span>');
-			});
-			if (!navigator.userAgent.match(new RegExp(knownSafeSingle.join('|'), 'gi')) && $testDiv.find('.flat').width() === $testDiv.find('.sharp').width() && $testDiv.find('.flat').width() === $testDiv.find('.natural').width()) { //if these are the same width, we'll guess that natural, sharp, and flat glyphs don't exist in the user's local font
-				accidentals.flat.safe = false;
-				accidentals.sharp.safe = false;
-				accidentals.natural.safe = false;
-			}
-			if ($testDiv.find('.doubleFlat').width() === $testDiv.find('.doubleSharp').width()) { //consider these separately, since they are more likely to be missing
-				accidentals.doubleFlat.safe = false;
-				accidentals.doubleSharp.safe = false;
-			}
-			$testDiv.remove();
-		}
 
 		function recursiveReplace(node, search, replace) {
 			var newContents;
@@ -108,31 +83,29 @@
 			var $this = $(this);
 			forEachIn(options.inputTypes, function (k, v) {
 				forEachIn(accidentals, function (k2, v2) {
-					if (accidentals[k2].safe) {
-						$this.contents().each(function () {
-							var search = new RegExp('\\b(' + options.noteNames + ')(' + v2[v] + ')(?=(s)?(\\b|\\s|\\W|$))', 'g' + options.caseSensitive),
-								txt,
-								replace = function ($0, $1) {
-									var note = $1,
-										symbol = v2.replacement,
-										output;
-									if (options.noteNameWrap) {
-										note = $(options.noteNameWrap).text(note).wrap('<div />').parent().html();
-									}
-									if (options.accidentalWrap) {
-										txt = options.accidentalClassBase ? v2.text : v2.text.replace(/^-/, '');
-										symbol = $(options.accidentalWrap).addClass(options.accidentalClassBase + txt).text(symbol).wrap('<div />').parent().html();
-									}
-									if (options.outerWrap) {
-										output = $(options.outerWrap).html(note + symbol).wrap('<div />').parent().html();
-									} else {
-										output = note + symbol;
-									}
-									return output;
-								};
-							recursiveReplace(this, search, replace);
-						});
-					}
+					$this.contents().each(function () {
+						var search = new RegExp('\\b(' + options.noteNames + ')(' + v2[v] + ')(?=(s)?(\\b|\\s|\\W|$))', 'g' + options.caseSensitive),
+							txt,
+							replace = function ($0, $1) {
+								var note = $1,
+									symbol = v2.replacement,
+									output;
+								if (options.noteNameWrap) {
+									note = $(options.noteNameWrap).text(note).wrap('<div />').parent().html();
+								}
+								if (options.accidentalWrap) {
+									txt = options.accidentalClassBase ? v2.text : v2.text.replace(/^-/, '');
+									symbol = $(options.accidentalWrap).addClass(options.accidentalClassBase + txt).text(symbol).wrap('<div />').parent().html();
+								}
+								if (options.outerWrap) {
+									output = $(options.outerWrap).html(note + symbol).wrap('<div />').parent().html();
+								} else {
+									output = note + symbol;
+								}
+								return output;
+							};
+						recursiveReplace(this, search, replace);
+					});
 				});
 			});
 		});
